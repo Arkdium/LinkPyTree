cdd247c4fc47e20bb68d720d02aa77dafd931836
Arkdium <arkdium@arkdium.dev>
2024-09-08 22:25:07 +0100

    Adjust Links

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.info('Motorista TVDE no Porto.')
 
 icon_size = 50
 
-st_button('instagram', 'https://instagram.com/ju.uber.pt', 'Me segue no Insta!', icon_size)
+st_button('instagram', 'https://www.instagram.com/ju.uberporto?igsh=MWQ3b3o2emxneWV3eg==', 'Me segue no Insta!', icon_size)
 st_button('whatsapp', 'wa.me/351932676539', 'Fale comigo no Whatsapp', icon_size)
-st_button('whatsapp', 'wa.me/351932676539', 'Entre para nosso grupo exclusivo de Meninas TVDE', icon_size)
+st_button('whatsapp', 'https://chat.whatsapp.com/HczP5bUqL1l7YtIwE1LMkF', 'Entre para nosso grupo exclusivo de Meninas TVDE', icon_size)
 #st_button('telegram', 't.me/', 'Fale comigo no Telegram', icon_size)
 #st_button('linkedin', 'https://www.linkedin.com/in/', 'Follow me on LinkedIn', icon_size)
 

e78e9c49e19b9dd21919d9afb3c959062cd66875
Arkdium <arkdium@arkdium.dev>
2024-09-08 23:47:58 +0100

    Adjust Links

--- st_functions.py
+++ st_functions.py
@@ -18,20 +18,24 @@ def st_button(icon, url, label, iconsize):
         # Codifica a imagem em base64
         icon_base64 = get_base64_image(icon_path)
         button_code = f'''
-        <p style="text-align: center;">
+        <p style="text-align: center; width: 100%;">
             <a href="{url}" target="_blank" class="btn btn-outline-primary btn-lg" type="button" aria-pressed="true" style="
-                display: block;
+                display: block;  
                 align-items: center; 
                 justify-content: center; 
                 text-decoration: none; 
                 padding: 10px 20px; 
                 border-radius: 8px; 
-                border: 2px solid #333333; 
-                background: linear-gradient(90deg, #4169E1, #333333);  /* Gradiente de azul royal para cinza escuro */
-                color: white; 
-                width: 100%;  /* Largura adaptável */
+                background: linear-gradient(90deg, #4169E1, #333333);  /* Gradiente de fundo */
+                color: white;  /* Texto sempre branco */
+                width: 100%;  /* Largura total */
                 font-size: 16px;  /* Tamanho do texto */
-                margin: 10px auto; /* Centraliza o botão */
+                margin: 10px 0;  /* Margem centralizada */
+                text-align: center;
+                border: 2px solid transparent; /* Define a borda para começar como transparente */
+                background-origin: border-box;
+                background-clip: padding-box, border-box;
+                box-shadow: 0px 0px 0px 2px transparent;
                 ">
                 <img src="data:image/png;base64,{icon_base64}" width="{iconsize}" height="{iconsize}" style="margin-right: 10px;">
                 {label}
@@ -39,19 +43,23 @@ def st_button(icon, url, label, iconsize):
         </p>'''
     else:
         button_code = f'''
-        <p style="text-align: center;">
+        <p style="text-align: center; width: 100%;">
             <a href="{url}" target="_blank" class="btn btn-outline-primary btn-lg" type="button" aria-pressed="true" style="
-                display: inline-block; 
+                display: block;  
                 padding: 10px 20px; 
                 border-radius: 8px; 
-                border: 2px solid #4169E1; 
-                background: linear-gradient(90deg, #4169E1, #333333);  /* Gradiente de azul royal para cinza escuro */
-                color: white; 
+                background: linear-gradient(90deg, #4169E1, #333333);  /* Gradiente de fundo */
+                color: white;  /* Texto sempre branco */
                 text-align: center; 
                 text-decoration: none; 
-                width: 100%;  /* Largura adaptável */
+                width: 100%;  /* Largura total */
                 font-size: 16px;  /* Tamanho do texto */
-                margin: 10px auto; /* Centraliza o botão */
+                margin: 10px 0; /* Margem centralizada */
+                text-align: center;
+                border: 2px solid transparent;
+                background-origin: border-box;
+                background-clip: padding-box, border-box;
+                box-shadow: 0px 0px 0px 2px transparent;
                 ">
                 {label}
             </a>

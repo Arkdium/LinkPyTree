d961da40c2d64e595197bb5453dc4c0284be73a9
Arkdium <arkdium@arkdium.dev>
2024-09-09 00:07:19 +0100

    Adjust Links

--- st_functions.py
+++ st_functions.py
@@ -27,6 +27,7 @@ def add_custom_css():
         }}
                 
         .css-1w6rlcb.e12sa3f30, css-17z2rne.e13vu3m50 {{
+            display: block; 
             text-align: center;
             margin: 0 auto;
             width: 80%;
